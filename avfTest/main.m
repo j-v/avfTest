36bc2ac7a09dab27708dfdbf05a9906b163c3671
Daniel M Karlsson <danielmkarlsson@Daniels-Mac-mini.local>
2013-03-21 19:48:59 +0100

    Initial Commit

---
+++ avfTest/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  avfTest
+//
+//  Created by Daniel M Karlsson on 3/21/13.
+//  Copyright (c) 2013 Jon Volkmar. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
